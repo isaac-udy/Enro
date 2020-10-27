e20e19680a8be09ee3a7e4ea73a628c20ae8a867
Isaac <isaac.udy@gmail.com>
2020-10-27 22:44:31 +1300

    Add example dialog fragment (hidden) for testing purposes in the example project

---
+++ example/src/main/java/nav/enro/example/ExampleDialogFragment.kt
@@ -0,0 +1,50 @@
+package nav.enro.example
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.fragment_example_dialog.*
+import nav.enro.annotations.NavigationDestination
+import nav.enro.core.*
+
+@Parcelize
+class ExampleDialogKey(val number: Int = 1) : NavigationKey
+
+@NavigationDestination(ExampleDialogKey::class)
+class ExampleDialogFragment : DialogFragment() {
+
+    private val navigation by navigationHandle<ExampleDialogKey>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_example_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        exampleDialogNumber.text = navigation.key.number.toString()
+
+        exampleDialogForward.setOnClickListener {
+            navigation.forward(ExampleDialogKey(navigation.key.number + 1))
+        }
+
+        exampleDialogReplace.setOnClickListener {
+            navigation.replace(ResultExampleKey())
+        }
+
+        exampleDialogClose.setOnClickListener {
+            navigation.close()
+        }
+    }
+}
