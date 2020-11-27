b1be7aab6e834ac5adc5e81f288b2e83ae9d59d8
Isaac <isaac.udy@gmail.com>
2020-11-27 23:48:50 +1300

    Delete publish.bat and publish-local.bat

--- publish-local.bat
+++
@@ -1,8 +0,0 @@
-gradlew :enro-annotations:publishToMavenLocal &&^
-gradlew :enro-processor:publishToMavenLocal &&^
-gradlew :enro-core:publishToMavenLocal &&^
-gradlew :enro-result:publishToMavenLocal &&^
-gradlew :enro-viewmodel:publishToMavenLocal &&^
-gradlew :enro-multistack:publishToMavenLocal &&^
-gradlew :enro-masterdetail:publishToMavenLocal &&^
-gradlew :enro:publishToMavenLocal
--- publish.bat
+++
@@ -1,8 +0,0 @@
-gradlew :enro-annotations:publish &&^
-gradlew :enro-processor:publish &&^
-gradlew :enro-core:publish &&^
-gradlew :enro-result:publish &&^
-gradlew :enro-viewmodel:publish &&^
-gradlew :enro-multistack:publish &&^
-gradlew :enro-masterdetail:publish &&^
-gradlew :enro:publish
