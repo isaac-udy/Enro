73e539b12a58f02fff24aa818355ce042d24012e
Isaac Udy <isaac.udy@xero.com>
2022-03-04 14:36:50 +1300

    Provide an additional resultId parameter using rememberSavable in the @Composable registerForNavigationResult to allow lists of items to uniquely identify result channels

--- enro-core/src/main/java/dev/enro/core/compose/ComposableNavigationResult.kt
+++ enro-core/src/main/java/dev/enro/core/compose/ComposableNavigationResult.kt
@@ -4,9 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisallowComposableCalls
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import dev.enro.core.result.EnroResult
 import dev.enro.core.result.EnroResultChannel
 import dev.enro.core.result.internal.ResultChannelImpl
+import java.util.*
 
 
 @Composable
@@ -14,11 +16,15 @@ inline fun <reified T: Any> registerForNavigationResult(
     noinline onResult: @DisallowComposableCalls (T) -> Unit
 ): EnroResultChannel<T> {
     val navigationHandle = navigationHandle()
-    val resultChannel = remember {
+    val resultId = rememberSaveable {
+        UUID.randomUUID().toString()
+    }
+    val resultChannel = remember(onResult) {
         ResultChannelImpl(
             navigationHandle = navigationHandle,
             resultType = T::class.java,
-            onResult = onResult
+            onResult = onResult,
+            resultId = resultId
         )
     }
 
