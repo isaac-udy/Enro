e502f47eea47aff88fa29e33db5d50d98cfb5391
Isaac Udy <isaac.udy@gmail.com>
2022-05-20 02:12:50 +1200

    Made ResultChannelId public

--- enro-core/src/main/java/dev/enro/core/result/internal/ResultChannelId.kt
+++ enro-core/src/main/java/dev/enro/core/result/internal/ResultChannelId.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-internal data class ResultChannelId(
+data class ResultChannelId(
     val ownerId: String,
     val resultId: String
 ) : Parcelable
