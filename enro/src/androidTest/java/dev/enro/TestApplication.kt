cd404a1121d6570a930ab79eac16aabbcd4884df
Isaac Udy <isaac.udy@gmail.com>
2022-07-27 15:14:38 +1200

    Add Hilt to the tests, add tests to check ViewModel creation using Hilt and Enro/NavigationHandles in the ViewModel

--- enro/src/androidTest/java/dev/enro/TestApplication.kt
+++ enro/src/androidTest/java/dev/enro/TestApplication.kt
@@ -1,15 +1,31 @@
 package dev.enro
 
 import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.CustomTestApplication
 import dev.enro.annotations.NavigationComponent
 import dev.enro.core.controller.NavigationApplication
+import dev.enro.core.controller.createNavigationComponent
 import dev.enro.core.controller.navigationController
 import dev.enro.core.plugins.EnroLogger
 
 @NavigationComponent
-class TestApplication : Application(), NavigationApplication {
+open class TestApplication : Application(), NavigationApplication {
     override val navigationController = navigationController {
         plugin(EnroLogger())
         plugin(TestPlugin)
+    }
+}
+@CustomTestApplication(TestApplication::class)
+interface HiltTestApplication
+
+class HiltTestApplicationRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication_Application::class.java.name, context).apply {
+            navigationController.addComponent(createNavigationComponent {
+                TestApplicationNavigation().execute(this)
+            })
+        }
     }
 }
---
+++ enro/src/androidTest/java/dev/enro/viewmodel/ViewModelCreationTests.kt
@@ -0,0 +1,183 @@
+package dev.enro.viewmodel
+
+import android.app.Application
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.test.core.app.ActivityScenario
+import androidx.test.platform.app.InstrumentationRegistry
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dev.enro.*
+import dev.enro.annotations.ExperimentalComposableDestination
+import dev.enro.annotations.NavigationDestination
+import dev.enro.core.NavigationKey
+import dev.enro.core.compose.EnroContainer
+import dev.enro.core.compose.composableManger
+import dev.enro.core.forward
+import dev.enro.core.getNavigationHandle
+import dev.enro.core.navigationHandle
+import junit.framework.TestCase.assertTrue
+import kotlinx.parcelize.Parcelize
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+@HiltAndroidTest
+class ViewModelCreationTests {
+
+    @get:Rule
+    val hilt = HiltAndroidRule(this)
+
+    @Test
+    fun whenActivityFragmentComposable_requestHiltInjectedViewModels_thenViewModelsAreCreated() {
+        ActivityScenario.launch(DefaultActivity::class.java)
+
+        expectContext<DefaultActivity, NavigationKey>()
+            .navigation
+            .forward(ContainerActivity.Key())
+
+        expectContext<ContainerActivity, ContainerActivity.Key>()
+            .apply {
+                InstrumentationRegistry.getInstrumentation().runOnMainSync {
+                    context.viewModel.hashCode()
+                }
+            }
+            .navigation
+            .forward(ContainerFragment.Key())
+
+        val fragment = expectContext<ContainerFragment, ContainerFragment.Key>()
+
+        fragment
+            .apply {
+                InstrumentationRegistry.getInstrumentation().runOnMainSync {
+                    context.viewModel.hashCode()
+                }
+            }
+            .navigation
+            .forward(Compose.Key())
+
+        // TODO: Once Enro 2.0 is released, this hacky way of checking the current top composable can be removed
+        val activeNavigation = waitOnMain {
+            fragment.context.composableManger.activeContainer?.activeContext?.getNavigationHandle()
+        }
+        Thread.sleep(1000)
+        assertTrue(activeNavigation.key is Compose.Key)
+    }
+
+    @AndroidEntryPoint
+    @NavigationDestination(ContainerActivity.Key::class)
+    class ContainerActivity : TestActivity() {
+
+        val viewModel by enroViewModels<TestViewModel>()
+        private val navigation by navigationHandle<ContainerActivity.Key> {
+            container(primaryFragmentContainer) {
+                it is ContainerFragment.Key
+            }
+        }
+
+        @Parcelize
+        class Key : NavigationKey
+
+        @HiltViewModel
+        class TestViewModel @Inject constructor(
+            val useCaseOne: ExampleDependencies.UseCaseOne,
+            val useCaseTwo: ExampleDependencies.UseCaseTwo,
+            val application: Application,
+            val savedStateHandle: SavedStateHandle
+        ): ViewModel() {
+            val navigation by navigationHandle<Key>()
+        }
+    }
+
+    @AndroidEntryPoint
+    @NavigationDestination(ContainerFragment.Key::class)
+    class ContainerFragment : androidx.fragment.app.Fragment() {
+
+        val viewModel by enroViewModels<TestViewModel>()
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View {
+            return ComposeView(requireContext()).apply {
+                setContent {
+                    EnroContainer(
+                        modifier = Modifier.size(width = 200.dp, height = 200.dp)
+                    )
+                }
+            }
+        }
+
+        @Parcelize
+        class Key : NavigationKey
+
+        @HiltViewModel
+        class TestViewModel @Inject constructor(
+            val useCaseOne: ExampleDependencies.UseCaseOne,
+            val useCaseTwo: ExampleDependencies.UseCaseTwo,
+            val application: Application,
+            val savedStateHandle: SavedStateHandle
+        ): ViewModel() {
+            val navigation by navigationHandle<Key>()
+        }
+    }
+
+
+    object Compose {
+        @Composable
+        @ExperimentalComposableDestination
+        @NavigationDestination(Compose.Key::class)
+        fun Draw() {
+            val viewModel = viewModel<TestViewModel>()
+
+            Text("Text with ${viewModel.navigation.key}")
+        }
+
+        @Parcelize
+        class Key : NavigationKey
+
+        @HiltViewModel
+        class TestViewModel @Inject constructor(
+            val useCaseOne: ExampleDependencies.UseCaseOne,
+            val useCaseTwo: ExampleDependencies.UseCaseTwo,
+            val application: Application,
+            val savedStateHandle: SavedStateHandle
+        ): ViewModel() {
+            val navigation by navigationHandle<Key>()
+        }
+    }
+}
+
+object ExampleDependencies {
+
+    @Singleton
+    class RepositoryOne @Inject constructor() {}
+
+    @Singleton
+    class RepositoryTwo @Inject constructor() {}
+
+    class UseCaseOne @Inject constructor(
+        val repositoryOne: RepositoryOne
+    ) {}
+
+    class UseCaseTwo @Inject constructor(
+        val repositoryOne: RepositoryOne,
+        val repositoryTwo: RepositoryTwo
+    ) {}
+}
