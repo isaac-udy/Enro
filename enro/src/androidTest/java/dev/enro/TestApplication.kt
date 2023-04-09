9a290166ade4c405d0e28dd49220f27de1c3ace2
Isaac Udy <isaac.udy@gmail.com>
2023-04-09 17:56:32 +1200

    Updated activity and fragment libraries, added a registered library leak for ReportFullyDrawnExecutorApi16Impl

--- enro/src/androidTest/java/dev/enro/TestApplication.kt
+++ enro/src/androidTest/java/dev/enro/TestApplication.kt
@@ -9,6 +9,8 @@ import dev.enro.core.destinations.ComposableDestinations
 import dev.enro.core.destinations.ManuallyBoundComposableScreen
 import dev.enro.core.plugins.EnroLogger
 import dev.enro.test.EnroTest
+import leakcanary.LeakCanary
+import shark.AndroidReferenceMatchers
 
 @NavigationComponent
 open class TestApplication : Application(), NavigationApplication {
@@ -18,5 +20,21 @@ open class TestApplication : Application(), NavigationApplication {
 
         composableDestination<ComposableDestinations.ManuallyBound> { ManuallyBoundComposableScreen() }
     }.also { EnroTest.disableAnimations(it) }
+
+    override fun onCreate() {
+        super.onCreate()
+        LeakCanary.config = LeakCanary.config.copy(
+            referenceMatchers = AndroidReferenceMatchers.appDefaults +
+                    AndroidReferenceMatchers.instanceFieldLeak(
+                        className = "androidx.activity.ComponentActivity\$ReportFullyDrawnExecutorApi16Impl",
+                        fieldName = "this\$0",
+                        description = "The ComponentActivity's ReportFullyFullyDrawnExecutorAPI16Impl can sometimes " +
+                                "leak with ActivityScenarios if they are recreated quickly and contain composables. " +
+                                "To reproduce a leak that shows this, add a setContentView { ... } to EmptyActivity, " +
+                                "launch EmptyActivity as an ActivityScenario, and then recreate it. Particularly on API 27 " +
+                                "this will cause a DetectLeaksAfterTestSuccess to fail the test",
+                    )
+        )
+    }
 }
 
