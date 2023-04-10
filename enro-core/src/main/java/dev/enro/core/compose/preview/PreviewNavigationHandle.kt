4a59d440ce27a93d7049b82aab479ccaed7f1364
Isaac Udy <isaac.udy@gmail.com>
2023-04-10 22:45:46 +1200

    Updated PreviewNavigationHandle to allow setting of directions

--- enro-core/src/main/java/dev/enro/core/compose/preview/PreviewNavigationHandle.kt
+++ enro-core/src/main/java/dev/enro/core/compose/preview/PreviewNavigationHandle.kt
@@ -3,8 +3,9 @@ package dev.enro.core.compose.preview
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.LifecycleOwner
 import dev.enro.core.*
 import dev.enro.core.compose.LocalNavigationHandle
 import dev.enro.core.controller.EnroDependencyScope
@@ -13,30 +14,26 @@ import dev.enro.core.internal.handle.NavigationHandleScope
 
 @ArchitectureException("PreviewNavigationHandle is a utility, and not a core part of Enro's API structure.")
 internal class PreviewNavigationHandle(
-    override val instruction: AnyOpenInstruction
+    override val instruction: AnyOpenInstruction,
+    private val lifecycleOwner: LifecycleOwner,
 ) : NavigationHandle {
     override val id: String = instruction.instructionId
     override val key: NavigationKey = instruction.navigationKey
     override val dependencyScope: EnroDependencyScope = NavigationHandleScope(NavigationController()).bind(this)
-
-    private val lifecycleRegistry by lazy {
-        LifecycleRegistry(this).apply {
-            handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        }
-    }
 
     override fun executeInstruction(navigationInstruction: NavigationInstruction) {
 
     }
 
     override val lifecycle: Lifecycle get() {
-        return lifecycleRegistry
+        return lifecycleOwner.lifecycle
     }
 }
 
 @Composable
 public fun <T : NavigationKey> EnroPreview(
     navigationKey: T,
+    navigationDirection: NavigationDirection? = null,
     content: @Composable () -> Unit
 ) {
     val isValidPreview = LocalInspectionMode.current && LocalNavigationHandle.current == null
@@ -45,8 +42,16 @@ public fun <T : NavigationKey> EnroPreview(
             "EnroPreview can only be used when LocalInspectionMode.current is true (i.e. inside of an @Preview function) and when there is no LocalNavigationHandle already"
         )
     }
+    val lifecycleOwner = LocalLifecycleOwner.current
     CompositionLocalProvider(
-        LocalNavigationHandle provides PreviewNavigationHandle(NavigationInstruction.DefaultDirection(navigationKey))
+        LocalNavigationHandle provides PreviewNavigationHandle(
+            instruction = when(navigationDirection) {
+                NavigationDirection.Push -> NavigationInstruction.Push(navigationKey as NavigationKey.SupportsPush)
+                NavigationDirection.Present -> NavigationInstruction.Present(navigationKey as NavigationKey.SupportsPresent)
+                else -> NavigationInstruction.DefaultDirection(navigationKey)
+            },
+            lifecycleOwner = lifecycleOwner
+        )
     ) {
         content()
     }
