04248ccd81885b5033acf86ac4e178bedd3c94da
Isaac Udy <isaac.udy@gmail.com>
2023-07-15 01:02:26 +1200

    Move build verification testing to a Unit test

--- tests/application/build.gradle.kts
+++ tests/application/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
 
     implementation(libs.material)
 
+    testImplementation(libs.testing.junit)
+
     androidTestImplementation(project(":enro-test"))
 
     androidTestImplementation(libs.testing.junit)
@@ -58,89 +60,4 @@ dependencies {
     androidTestImplementation(libs.testing.androidx.runner)
 
     androidTestImplementation(libs.testing.androidx.compose)
-}
-
-class BuildTests(val task: Task) {
-    val tests = mutableListOf<Pair<String, Throwable?>>()
-}
-
-fun BuildTests.create(
-    name: String,
-    block: () -> Unit
-) {
-    fun runBuild() {
-        val home = System.getProperty("java.home")
-        exec {
-            environment("JAVA_HOME", home)
-            commandLine(
-                "$rootDir/gradlew",
-                ":tests:application:assembleDebug",
-            )
-        }
-    }
-
-    task.doFirst {
-        rootProject.exec {
-            commandLine(
-                "git",
-                "stash",
-                "push",
-                "--include-untracked"
-            )
-        }
-        val result = runCatching {
-            runBuild()
-            block()
-            runBuild()
-        }
-        rootProject.exec {
-            commandLine(
-                "git",
-                "reset",
-                "--hard",
-            )
-        }
-        rootProject.exec {
-            commandLine(
-                "git",
-                "stash",
-                "pop",
-            )
-        }
-        tests.add(name to result.exceptionOrNull())
-    }
-}
-
-task("testApplicationBuildTests") {
-    val suite = BuildTests(this)
-    suite.create("Renamed destination and file in application") {
-        val editableFile = file("./src/main/java/dev/enro/tests/application/TestApplicationEditableDestination.kt")
-        val editedContent = """
-package dev.enro.tests.application
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import dev.enro.annotations.NavigationDestination
-import dev.enro.core.NavigationKey
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-internal class TestApplicationEditableDestination_Edited : NavigationKey.SupportsPush
-
-@Composable
-@NavigationDestination(TestApplicationEditableDestination_Edited::class)
-internal fun TestApplicationEditableScreen_Edited() {
-    Text("Test Screen (Edited)")
-}
-""".trimIndent()
-
-        editableFile.writeText(editedContent)
-        val renamedFile = file("./src/main/java/dev/enro/tests/application/TestApplicationEditableDestination_Edited.kt")
-        editableFile.renameTo(renamedFile)
-    }
-
-    val failed = suite.tests.filter { it.second != null }
-    if (failed.isNotEmpty()) {
-        error("Failed tests:\n${failed.joinToString(separator = "\n") { it.first }}")
-    }
 }
