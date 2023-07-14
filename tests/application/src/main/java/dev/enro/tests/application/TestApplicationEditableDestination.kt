839b549ef3c46587295bfa0ca95f527bf1af8d47
Isaac Udy <isaac.udy@gmail.com>
2023-07-15 02:13:01 +1200

    Add comments to the TestApp/TestModuleEditableDestination classes

--- tests/application/src/main/java/dev/enro/tests/application/TestApplicationEditableDestination.kt
+++ tests/application/src/main/java/dev/enro/tests/application/TestApplicationEditableDestination.kt
@@ -6,6 +6,13 @@ import dev.enro.annotations.NavigationDestination
 import dev.enro.core.NavigationKey
 import kotlinx.parcelize.Parcelize
 
+
+/**
+ * This NavigationKey and associated NavigationDestination are used from inside of the
+ * ProjectChangeTests of the :tests:application module. This file is modified/renamed/deleted/etc
+ * as part of an incremental build which is run during the test to check that the KSP processor
+ * correctly generates the associated binding files and compiles correctly.
+ */
 @Parcelize
 internal class TestApplicationEditableDestination : NavigationKey.SupportsPush
 
