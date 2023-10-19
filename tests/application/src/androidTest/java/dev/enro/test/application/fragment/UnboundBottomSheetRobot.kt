551556effa1a4a764b24d56e08ae5cc5ce3c25e8
Isaac Udy <isaac.udy@gmail.com>
2023-10-19 21:01:09 +1300

    Manually focus the BottomSheet's View in UnboundBottomSheetRobot, as this test appears to fail on API 30 otherwise

--- tests/application/src/androidTest/java/dev/enro/test/application/fragment/UnboundBottomSheetRobot.kt
+++ tests/application/src/androidTest/java/dev/enro/test/application/fragment/UnboundBottomSheetRobot.kt
@@ -36,6 +36,11 @@ class UnboundBottomSheetRobot(
     }
 
     fun closeWithBackPress() {
+        // on API 30 Espresso seems to fail to call "pressBack" unless the fragment's View is
+        // manually requested for focus. This does not occur on API 23, 27 or 33. I believe this
+        // is a bug in Espresso/API 30 specifically.
+        fragment.requireView().requestFocus()
+
         Espresso.pressBack()
         composeRule.waitUntil(5_000) {
             !fragment.isAdded && fragment.activity == null
