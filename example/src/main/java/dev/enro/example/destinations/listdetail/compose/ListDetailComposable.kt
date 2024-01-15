4d9899039e9381e55497e479c1035a52f29fd6a7
Isaac Udy <isaac.udy@gmail.com>
2024-01-16 07:11:13 +1300

    Formatting and import updates

--- example/src/main/java/dev/enro/example/destinations/listdetail/compose/ListDetailComposable.kt
+++ example/src/main/java/dev/enro/example/destinations/listdetail/compose/ListDetailComposable.kt
@@ -2,18 +2,20 @@ package dev.enro.example.destinations.listdetail.compose
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.platform.LocalConfiguration
 import dev.enro.annotations.NavigationDestination
-import dev.enro.core.*
+import dev.enro.core.NavigationContainerKey
+import dev.enro.core.NavigationKey
 import dev.enro.core.compose.rememberNavigationContainer
-import dev.enro.core.container.*
+import dev.enro.core.container.EmptyBehavior
 import kotlinx.parcelize.Parcelize
-import java.util.*
 
 @Parcelize
 class ListDetailComposable : NavigationKey.SupportsPush
@@ -35,7 +37,7 @@ fun ListDetailComposeScreen() {
         emptyBehavior = EmptyBehavior.AllowEmpty,
         accept = { it is DetailComposable }
     )
-    
+
     val isLandscape = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
     if (isLandscape) {
         Row(
@@ -44,13 +46,15 @@ fun ListDetailComposeScreen() {
                 .fillMaxSize()
         ) {
             Box(
-                modifier = Modifier.weight(1f, true)
+                modifier = Modifier
+                    .weight(1f, true)
                     .clipToBounds()
             ) {
                 listContainerController.Render()
             }
             Box(
-                modifier = Modifier.weight(1f, true)
+                modifier = Modifier
+                    .weight(1f, true)
                     .clipToBounds()
             ) {
                 detailContainerController.Render()
