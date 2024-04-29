9b76ddd7c84cc28ae69854f5d746290a0abf1e5a
Isaac Udy <isaac.udy@gmail.com>
2024-04-29 21:16:26 +1200

    Add comments

--- enro-core/src/main/java/dev/enro/destination/compose/ComposableDestinationExtensions.kt
+++ enro-core/src/main/java/dev/enro/destination/compose/ComposableDestinationExtensions.kt
@@ -11,6 +11,9 @@ import dev.enro.animation.NavigationAnimation
 import dev.enro.annotations.AdvancedEnroApi
 import dev.enro.core.navigationContext
 
+/**
+ * Causes the ComposableDestination's transition to immediately finish
+ */
 @AdvancedEnroApi
 public fun ComposableDestination.finishTransition() {
     val transition = owner.transitionState
@@ -19,11 +22,18 @@ public fun ComposableDestination.finishTransition() {
     }
 }
 
+/**
+ * Gets the transition for the ComposableDestination
+ */
 @AdvancedEnroApi
 public fun ComposableDestination.getTransition() : Transition<Boolean> {
     return owner.transition
 }
 
+/**
+ * Gets the transition for the current navigationContext. This is only valid if the current context is a ComposableDestination,
+ * and will otherwise throw an exception.
+ */
 @AdvancedEnroApi
 public val navigationTransition: Transition<Boolean>
     @Composable
@@ -32,12 +42,21 @@ public val navigationTransition: Transition<Boolean>
         return destination.getTransition()
     }
 
+/**
+ * Overrides the navigation animations for the destination. This is primarily useful when the animations for a given destination
+ * should not be applied from the navigation container. The cases for this are rare, and are generally edge case situations.
+ *
+ * For example, this is used in the DialogDestination Composable, because that Composable expects that a Dialog will handle it's
+ * own entering and exiting animations, rather than attempting to animate the Composable that is holding the Dialog.
+ */
 @Composable
 @AdvancedEnroApi
 public fun OverrideNavigationAnimations(
     enter: EnterTransition,
     exit: ExitTransition,
 ) {
+    // If we are in inspection mode, we need to ignore this call, as it relies on items like navigationContext
+    // which are only available in actual running applications
     val isInspection = LocalInspectionMode.current
     if (isInspection) return
 
