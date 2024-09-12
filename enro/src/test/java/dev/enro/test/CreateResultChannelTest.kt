9a2a30c1b110c6681d06e5b7a542bd99ee51702d
Isaac Udy <isaac.udy@gmail.com>
2024-09-12 23:44:43 +1200

    Fix result channel tests

--- enro/src/test/java/dev/enro/test/CreateResultChannelTest.kt
+++ enro/src/test/java/dev/enro/test/CreateResultChannelTest.kt
@@ -12,11 +12,9 @@ import dev.enro.core.result.NavigationResultScope
 import dev.enro.core.result.internal.ResultChannelId
 import dev.enro.core.result.internal.ResultChannelImpl
 import dev.enro.core.result.registerForNavigationResult
-import dev.enro.core.result.registerForNavigationResultWithKey
 import dev.enro.test.extensions.putNavigationHandleForViewModel
 import dev.enro.viewmodel.withNavigationHandle
 import kotlinx.parcelize.Parcelize
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Rule
 import org.junit.Test
@@ -24,168 +22,38 @@ import org.junit.Test
 @Parcelize
 private class ResultChannelTestKey : NavigationKey.SupportsPresent
 
-/**
- * CreateResultChannel has two different methods which allow the creation of a result channel.
- * One method allows the onResult/onClosed lambdas to accept a NavigationKey, and one method
- * hides the NavigationKey from the lambda. This is the basis of the `key vs. no key` distinction
- * in these tests, as channels created through either method should be uniquely identifiable.
- */
+
 class CreateResultChannelTest {
     @Rule
     @JvmField
     val enroTestRule = EnroTestRule()
 
     @Test
-    fun resultChannelsAreUniquelyIdentifiable_keyVsNoKey() {
-        val createResultChannel = createTestNavigationHandle(ResultChannelTestKey())
-            .dependencyScope
-            .get<CreateResultChannel>()
-
-        val channelOne = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { },
-            onResult = { _ -> },
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        val channelTwo = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { _ -> },
-            onResult = { _, _ -> },
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        assertNotEquals(channelOne.id, channelTwo.id)
-    }
-
-    @Test
-    fun resultChannelsAreUniquelyIdentifiable_keyVsKey() {
-        val createResultChannel = createTestNavigationHandle(ResultChannelTestKey())
-            .dependencyScope
-            .get<CreateResultChannel>()
-
-        val channelOne = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { _ -> },
-            onResult = { _, _ -> },
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        val channelTwo = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { _ -> },
-            onResult = { _, _ -> },
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        assertNotEquals(channelOne.id, channelTwo.id)
-    }
-
-    @Test
-    fun resultChannelsAreUniquelyIdentifiable_noKeyVsNoKey() {
-        val createResultChannel = createTestNavigationHandle(ResultChannelTestKey())
-            .dependencyScope
-            .get<CreateResultChannel>()
-
-        val channelOne = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { },
-            onResult = {  _ -> },
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        val channelTwo = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { },
-            onResult = {  _ -> },
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        assertNotEquals(channelOne.id, channelTwo.id)
-    }
-
-    @Test
-    fun resultChannelsAreUniquelyIdentifiable_keyWithRepeatedLambda() {
-        val createResultChannel = createTestNavigationHandle(ResultChannelTestKey())
-            .dependencyScope
-            .get<CreateResultChannel>()
-
-        val result: NavigationResultScope<String, NavigationKey.WithResult<String>>.(NavigationKey, String) -> Unit = { _, _ -> }
-        val channelOne = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { },
-            onResult = result,
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        val channelTwo = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { },
-            onResult = result,
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        assertNotEquals(channelOne.id, channelTwo.id)
-    }
-
-    @Test
-    fun resultChannelsAreUniquelyIdentifiable_noKeyWithRepeatedLambda() {
-        val createResultChannel = createTestNavigationHandle(ResultChannelTestKey())
-            .dependencyScope
-            .get<CreateResultChannel>()
-
-        val result: NavigationResultScope<String, NavigationKey.WithResult<String>>.(String) -> Unit = {}
-        val channelOne = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { },
-            onResult = result,
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        val channelTwo = createResultChannel.invoke(
-            resultType = String::class,
-            onClosed = { },
-            onResult = result,
-            additionalResultId = "",
-        ) as ResultChannelImpl
-
-        assertNotEquals(channelOne.id, channelTwo.id)
-    }
-
-    @Test
-    fun resultChannelsAreNotUniquelyIdentifiableInForLoop() {
-        val createResultChannel = createTestNavigationHandle(ResultChannelTestKey())
-            .dependencyScope
-            .get<CreateResultChannel>()
-
-        val result: NavigationResultScope<String, NavigationKey.WithResult<String>>.(String) -> Unit = {}
-        val channels = (0..2).map {
-            createResultChannel.invoke(
-                resultType = String::class,
-                onClosed = { },
-                onResult = result,
-                additionalResultId = "",
-            ) as ResultChannelImpl
+    fun resultChannelsAreUniquelyIdentifiableWithinViewModel() {
+        class ExampleOne : ViewModel() {
+            val channelOne by registerForNavigationResult<String> {  }
+            val channelTwo by registerForNavigationResult<String> {  }
         }
-        assertEquals(channels[0].id, channels[1].id)
+        putNavigationHandleForViewModel<ExampleOne>(ResultChannelTestKey())
+        val viewModel = ExampleOne()
+        assertNotEquals(viewModel.channelOne.internalId, viewModel.channelTwo.internalId)
     }
 
     @Test
-    fun resultChannelsAreUniquelyIdentifiableInForLoop_providedAdditionalId() {
+    fun resultChannelsAreUniquelyIdentifiableWithinViewModel_keyWithRepeatedLambda() {
         val createResultChannel = createTestNavigationHandle(ResultChannelTestKey())
             .dependencyScope
             .get<CreateResultChannel>()
 
-        val result: NavigationResultScope<String, NavigationKey.WithResult<String>>.(String) -> Unit = {}
-        val channels = (0..2).map {
-            createResultChannel.invoke(
-                resultType = String::class,
-                onClosed = { },
-                onResult = result,
-                additionalResultId = "loop@$it",
-            ) as ResultChannelImpl
+        val result: NavigationResultScope<String, NavigationKey.WithResult<String>>.(String) -> Unit = { }
+
+        class ExampleOne : ViewModel() {
+            val channelOne by registerForNavigationResult<String>(onResult = result)
+            val channelTwo by registerForNavigationResult<String>(onResult = result)
         }
-        assertNotEquals(channels[0].id, channels[1].id)
+        putNavigationHandleForViewModel<ExampleOne>(ResultChannelTestKey())
+        val viewModel = ExampleOne()
+        assertNotEquals(viewModel.channelOne.internalId, viewModel.channelTwo.internalId)
     }
 
     @Test
@@ -255,19 +123,19 @@ class TestResultIdsWithKeyViewModel : ViewModel() {
     var intOneResult: Int? = null
     var intTwoResult: Int? = null
 
-    val stringOne by registerForNavigationResultWithKey<String> { _, it ->
+    val stringOne by registerForNavigationResult<String> {
         stringOneResult = it
     }
 
-    val stringTwo by registerForNavigationResultWithKey<String> { _, it ->
+    val stringTwo by registerForNavigationResult<String> {
         stringTwoResult = it
     }
 
-    val intOne by registerForNavigationResultWithKey<Int> { _, it ->
+    val intOne by registerForNavigationResult<Int> {
         intOneResult = it
     }
 
-    val intTwo by registerForNavigationResultWithKey<Int> { _, it ->
+    val intTwo by registerForNavigationResult<Int> {
         intTwoResult = it
     }
 }
