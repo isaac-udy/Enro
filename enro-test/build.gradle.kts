19a2d4692865863d4cf1e92701081831e8f601ce
Isaac Udy <isaac.udy@gmail.com>
2025-03-06 22:47:19 +1300

    Update enro-test to be a KMP library

--- enro-test/build.gradle.kts
+++ enro-test/build.gradle.kts
@@ -1,29 +1,39 @@
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    id("configure-library")
     id("configure-publishing")
 }
-configureAndroidLibrary("dev.enro.test")
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     compilerOptions {
         jvmTarget.set(JvmTarget.JVM_21)
         freeCompilerArgs.add("-Xfriend-paths=../enro-core/src/main")
     }
 }
 
-dependencies {
-    api("dev.enro:enro-core:${project.enroVersionName}")
+kotlin {
+    explicitApi = ExplicitApiMode.Disabled
+    sourceSets {
+        desktopMain.dependencies {
+        }
+        commonMain.dependencies {
+        }
+        androidMain.dependencies {
+            api("dev.enro:enro-core:${project.enroVersionName}")
 
-    implementation(libs.androidx.core)
-    implementation(libs.androidx.appcompat)
+            implementation(libs.androidx.core)
+            implementation(libs.androidx.appcompat)
 
-    implementation(libs.testing.junit)
-    implementation(libs.testing.androidx.runner)
-    implementation(libs.testing.androidx.core)
-    implementation(libs.testing.androidx.espresso)
-    //noinspection FragmentGradleConfiguration
-    implementation(libs.testing.androidx.fragment)
+            implementation(libs.testing.junit)
+            implementation(libs.testing.androidx.runner)
+            implementation(libs.testing.androidx.core)
+            implementation(libs.testing.androidx.espresso)
+            //noinspection FragmentGradleConfiguration
+            implementation(libs.testing.androidx.fragment)
+
+        }
+    }
 }
+
