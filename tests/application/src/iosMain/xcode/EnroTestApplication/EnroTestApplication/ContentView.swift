e4694a8f544d03fcc6c9ed4fed22ed2bc61da38e
Isaac Udy <isaac.udy@gmail.com>
2025-04-24 13:33:03 +1200

    Add basic iOS framework for :tests:application

---
+++ tests/application/src/iosMain/xcode/EnroTestApplication/EnroTestApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnroTestApplication
+//
+//  Created by Isaac Udy on 24/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tests/application/src/iosMain/xcode/EnroTestApplication/EnroTestApplication/EnroTestApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnroTestApplicationApp.swift
+//  EnroTestApplication
+//
+//  Created by Isaac Udy on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct EnroTestApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
