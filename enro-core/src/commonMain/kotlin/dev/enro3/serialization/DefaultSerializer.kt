3af77555ba7b84bc84575a3cfb64d5f1a132c05d
Isaac Udy <isaac.udy@gmail.com>
2025-06-03 23:14:13 +1200

    Deleted DefaultSerializer as it was not used

--- enro-core/src/commonMain/kotlin/dev/enro3/serialization/DefaultSerializer.kt
+++
@@ -1,6 +0,0 @@
-package dev.enro3.serialization
-
-import dev.enro3.NavigationKey
-import kotlinx.serialization.KSerializer
-
-public expect inline fun <reified T : NavigationKey> NavigationKey.Companion.defaultSerializer(): KSerializer<T>
