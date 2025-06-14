6ed8ea5555dbbeec3c2e36bb9db4612ce5f721bb
Isaac Udy <isaac.udy@gmail.com>
2025-06-15 02:17:35 +1200

    Update configureEmulatorWtf to use local.properties instead of private.properties to find ewApiToken

--- buildSrc/src/main/kotlin/Project.configureEmulatorWtf.kt
+++ buildSrc/src/main/kotlin/Project.configureEmulatorWtf.kt
@@ -8,18 +8,18 @@ import java.util.*
 fun Project.configureEmulatorWtf(numShards: Int = 2) {
     extensions.configure<EwExtension> {
 
-        val privateProperties = Properties()
-        val privatePropertiesFile = rootProject.file("private.properties")
-        if (privatePropertiesFile.exists()) {
-            privateProperties.load(FileInputStream(rootProject.file("private.properties")))
+        val localProperties = Properties()
+        val localPropertiesFile = rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localProperties.load(FileInputStream(localPropertiesFile))
         }
 
         when {
             project.hasProperty("ewApiToken") -> {
                 token.set(project.properties["ewApiToken"].toString())
             }
-            privateProperties.hasProperty("ewApiToken") -> {
-                token.set(privateProperties["ewApiToken"].toString())
+            localProperties.hasProperty("ewApiToken") -> {
+                token.set(localProperties["ewApiToken"].toString())
             }
             else -> {
                 token.set(java.lang.System.getenv()["EW_API_TOKEN"])
