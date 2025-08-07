4275639ec231133f388277484dfea65ef508d0ec
Isaac Udy <isaac.udy@gmail.com>
2025-08-08 00:51:38 +1200

    Remove configure step for javadocs

--- buildSrc/src/main/kotlin/configurePublishing.kt
+++ buildSrc/src/main/kotlin/configurePublishing.kt
@@ -1,7 +1,4 @@
 
-import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
-import com.vanniktech.maven.publish.JavadocJar
-import com.vanniktech.maven.publish.KotlinJvm
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
@@ -94,23 +91,6 @@ private fun Project.configurePublishing(
             }
 
             coordinates(groupName, moduleName, versionName)
-
-            if (isAndroid) {
-                configure(
-                    AndroidSingleVariantLibrary(
-                        variant = "release",
-                        sourcesJar = true,
-                        publishJavadocJar = false
-                    )
-                )
-            } else {
-                configure(
-                    KotlinJvm(
-                        javadocJar = JavadocJar.Empty(),
-                        sourcesJar = true
-                    )
-                )
-            }
 
             pom {
                 name.set(moduleName)
