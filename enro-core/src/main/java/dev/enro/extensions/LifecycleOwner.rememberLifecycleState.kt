2b346bcb2c99c5c7aca7cbca00a6d47bcc3412ce
Isaac Udy <isaac.udy@gmail.com>
2025-08-07 23:49:45 +1200

    Fix linting issue

--- enro-core/src/main/java/dev/enro/extensions/LifecycleOwner.rememberLifecycleState.kt
+++ enro-core/src/main/java/dev/enro/extensions/LifecycleOwner.rememberLifecycleState.kt
@@ -1,23 +1,11 @@
 package dev.enro.extensions
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.currentStateAsState
 
 @Composable
 internal fun LifecycleOwner.rememberLifecycleState() : Lifecycle.State {
-    val activeState = remember(this, lifecycle.currentState) { mutableStateOf(lifecycle.currentState) }
-
-    DisposableEffect(this, activeState) {
-        val observer = LifecycleEventObserver { _, event ->
-            activeState.value = event.targetState
-        }
-        lifecycle.addObserver(observer)
-        onDispose { lifecycle.removeObserver(observer) }
-    }
-    return activeState.value
+    return lifecycle.currentStateAsState().value
 }
