a39609a118c4efc4e8d4e30e900f9e54d22d764b
Isaac Udy <isaac.udy@gmail.com>
2026-05-12 03:42:41 +1200

    Fix import error

--- enro-compat/src/androidMain/kotlin/dev/enro/core/parentContainer.kt
+++ enro-compat/src/androidMain/kotlin/dev/enro/core/parentContainer.kt
@@ -1,9 +1,9 @@
 package dev.enro.core
 
 import androidx.compose.runtime.Composable
-import dev.enro.ui.LocalNavigationContainerOrNull
+import dev.enro.ui.LocalNavigationContainer
 import dev.enro.ui.NavigationContainerState
 
 public val parentContainer: NavigationContainerState?
     @Composable
-    get() = LocalNavigationContainerOrNull.current
+    get() = LocalNavigationContainer.currentOrNull
